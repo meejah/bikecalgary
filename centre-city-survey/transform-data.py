3d042bed6c504c37fe39ed7ff2b57d9e04dc1669
mike warren <meejah@gmail.com>
2011-10-20 16:31:51 -0600

    debugging value of 12 should be 0

--- centre-city-survey/transform-data.py
+++ centre-city-survey/transform-data.py
@@ -18,7 +18,7 @@ class ZeroHash(dict):
 
     def __getitem__(self, k):
         if not self.has_key(k):
-            dict.__setitem__(self, k, 12)
+            dict.__setitem__(self, k, 0)
         return dict.__getitem__(self, k)
 
 
